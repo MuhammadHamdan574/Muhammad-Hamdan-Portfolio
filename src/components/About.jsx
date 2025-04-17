716cd56a4c8103c9a81232f391c7c96dcd0035dd
MuhammadHamdan574 <hamdan878790@gmail.com>
2025-04-17 11:04:25 +0500

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Hamdan, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          I hold a Bachelor in Software Engineering from Gift University,
+          completed in 2022. In 2024, I earned a Full-Stack Web Development
+          Certificate from Xprt Computing, where I gained hands-on experience
+          with modern web technologies. I also completed React – The Complete
+          Guide on Udemy in the same year, strengthening my front-end
+          development skills. Additionally, I studied JavaScript Algorithms and
+          Data Structures at Xprt Computing in 2023 to deepen my problem-solving
+          and coding efficiency.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in MERN Full Stack development, including MongoDB,
+          Express.js, React.js, and Node.js, with additional hands-on experience
+          in HTML, CSS, Tailwind CSS, and JavaScript for creating responsive and
+          modern interfaces. Skilled in building scalable backend solutions
+          using .NET Core and .NET Core MVC, and experienced in frontend
+          frameworks like Angular. Familiar with advanced UI libraries such as
+          ShadCN UI and Aceternity UI to craft elegant, user-friendly designs.
+          Experienced with essential software tools and technologies such as
+          Git, GitHub, VS Code, Postman, and Firebase. Strong grasp of design
+          principles, clean UI/UX practices, and responsive layouts.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Developed and deployed full-stack applications using MongoDB,
+          Express.js, React.js, and Node.js. Optimized front-end performance and
+          implemented RESTful APIs for dynamic data handling.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          Recognized as the Top Performer at Xprt Computing in 2023 for
+          delivering high-quality solutions and contributing to key product
+          launches. Earned a Certificate of Excellence in Web Development from
+          Xprt Computing in 2024 after completing over 300 hours of hands-on
+          projects.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Your Field] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/PortFolio.jsx
@@ -0,0 +1,85 @@
+import mongoDB from "../../public/mongodb.jpg";
+import express from "../../public/express.png";
+import reactjs from "../../public/reactjs.png";
+import nodejs from "../../public/node.png";
+import dotnetcore from "../../public/dotnetcore.png";
+
+function PortFolio() {
+  const cardItem = [
+    {
+      id: 1,
+      logo: mongoDB,
+      name: "MongoDB",
+      description:
+        "MongoDB, a flexible NoSQL database designed for scalability and performance.",
+    },
+    {
+      id: 2,
+      logo: express,
+      name: "Express",
+      description:
+        "Express.js, a minimal and fast Node.js web application framework.",
+    },
+    {
+      id: 3,
+      logo: reactjs,
+      name: "ReactJS",
+      description:
+        "ReactJS, a powerful library for building dynamic user interfaces.",
+    },
+    {
+      id: 4,
+      logo: nodejs,
+      name: "NodeJS",
+      description:
+        "NodeJS, a JavaScript runtime built for fast, scalable network applications.",
+    },
+    {
+      id: 5,
+      logo: dotnetcore,
+      name: "DotNet Core",
+      description:
+        ".Net Core, a high performance framework for building modern applications.",
+    },
+  ];
+
+  return (
+    <div
+      name="Portfolio"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">PortFolio</h1>
+        <span className="underline font-semibold">Featured Projects</span>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-3 my-5">
+          {cardItem.map(({ id, logo, name, description }) => (
+            <div
+              className="md:w-[300px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300"
+              key={id}
+            >
+              <img
+                src={logo}
+                className="w-[120px] h-[120px] p-1 rounded-full border-[2px] mx-auto"
+                alt={name}
+              />
+              <div className="text-center">
+                <div className="px-2 font-bold text-xl mb-2">{name}</div>
+                <p className="px-2 text-gray-700">{description}</p>
+              </div>
+              <div className="flex justify-center px-6 py-4 space-x-3">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
+                  Video
+                </button>
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded">
+                  Source code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortFolio;
